99ee4153884f4666dafda07927bfa1d9f57555c3
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-03 18:32:05 -0700

    Complete Prototype pattern

---
+++ swe/courses/nodejs_design_patterns/creational/prototype/index.js
@@ -0,0 +1,13 @@
+var scout_prototype = require('./scout_prototype');
+const Shopper = require('./Shopper');
+
+var alex = scout_prototype.clone();
+alex.name = 'Alex Banks';
+alex.addItemToList('slingshot');
+
+var eve = scout_prototype.clone();
+eve.name = 'Eve Porcello';
+eve.addItemToList('reading light');
+
+console.log(`${alex.name}: ${alex.shoppingList}`);
+console.log(`${eve.name}: ${eve.shoppingList}`);
