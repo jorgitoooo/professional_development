f8ad0496ec645bf28d2d9ae05202ad29d6e600ca
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-12 15:29:35 -0700

    Add script that imports/deletes sample data from db

---
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const { Tour } = require('../../models');
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+// Connect to DB
+const DB = process.env.MONGO_URI.replace('<PWD>', process.env.MONGO_PWD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Database connection successful.');
+  });
+
+// Import all tours
+async function importData() {
+  try {
+    const tours = JSON.parse(
+      fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+    );
+    await Tour.create(tours);
+    console.log('Data successfully imported!');
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit(0);
+}
+
+// Delete all tours
+async function deleteData() {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit(0);
+}
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+} else {
+  console.log('   node <path-to-script> (--import | --delete)');
+  process.exit(0);
+}
