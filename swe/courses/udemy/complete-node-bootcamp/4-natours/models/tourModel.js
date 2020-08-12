d8899f24ef9d592f2f292d8bcbec82193e733115
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-12 15:30:33 -0700

    Add central import point (index.js) for models

--- swe/courses/udemy/complete-node-bootcamp/4-natours/models/tourModel.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/models/tourModel.js
@@ -5,15 +5,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 3.0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
-  rating: {
-    type: Number,
-    default: 3.0,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
