dbbfa89e9cf304a0d42b26718a255da8aae5925e
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-12 16:45:34 -0700

    Add tour stats route

--- swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
@@ -12,6 +12,42 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRatings: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: STATUS.SUCCESS,
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: STATUS.FAIL,
+      message: err,
+    });
+  }
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const qBuilder = new QueryBuilder(Tour.find(), req.query)
