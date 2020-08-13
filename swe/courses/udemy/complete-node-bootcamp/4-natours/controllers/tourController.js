bc830fe28409e589a3dc1050bc8094bbffd147b9
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-12 17:24:02 -0700

    Add monthly plan route

--- swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
@@ -48,6 +48,59 @@ exports.getTourStats = async (req, res) => {
   }
 };
 
+exports.getMonthlyPlan = async (req, res) => {
+  const year = parseInt(req.params.year, 10);
+
+  try {
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTours: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: STATUS.SUCCESS,
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: STATUS.FAIL,
+      message: err,
+    });
+  }
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const qBuilder = new QueryBuilder(Tour.find(), req.query)
