af22089a60608fdb8db5b5d6c92b4bc2abaaaaa9
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-13 22:51:11 -0700

    Create user model and validators

---
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email address'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (pwd) {
+        // Validator only runs on create() & save()
+        return pwd === this.password;
+      },
+      message: 'Passwords do not match',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, /** salt=*/ 12);
+    this.passwordConfirm = undefined; // Only used to confirm equal pwd
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
