55623e01ed2aaecebd8f9db342d55cfbc56aedf6
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-14 00:12:45 -0700

    Implement get all users route

--- swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/userController.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/userController.js
@@ -1,27 +1,42 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+const { User } = require('../models');
+const { AppError } = require('../utils');
+const { CODE, STATUS } = require('../constants');
+const { catchAsync } = require('../utils');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // Submit response
+  res.status(CODE.OK).json({
+    status: STATUS.SUCCESS,
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!',
   });
 };
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
