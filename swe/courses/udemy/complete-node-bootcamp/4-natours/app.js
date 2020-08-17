e90f6abe20a534595e2ab438e74505c61ccd275f
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-17 13:10:18 -0700

    Set security headers using helmet

--- swe/courses/udemy/complete-node-bootcamp/4-natours/app.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const { errorController } = require('./controllers');
@@ -10,19 +11,27 @@ const { CODE } = require('./constants');
 const app = express();
 
 // 1) MIDDLEWARES
+// Set security headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
+// Limit request from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP. Please try again in an hour.',
 });
 app.use('/api', limiter);
+
+// Body parser, sets req.body with data from body of request
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
