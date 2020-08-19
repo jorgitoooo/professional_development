4f6dfdd3df21ac5139c6c005ce4c0fcb3590b435
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-19 12:20:47 -0700

    Modify import/delete data script

--- swe/courses/udemy/complete-node-bootcamp/4-natours/dev-data/data/import-dev-data.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/dev-data/data/import-dev-data.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const { Tour } = require('../../models');
+const { Tour, User, Review } = require('../../models');
 
 dotenv.config({ path: `${__dirname}/../../config.env` });
 
@@ -25,7 +25,15 @@ async function importData() {
     const tours = JSON.parse(
       fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
     );
+    const users = JSON.parse(
+      fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+    );
+    const reviews = JSON.parse(
+      fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+    );
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully imported!');
   } catch (err) {
     console.error(err);
@@ -37,6 +45,8 @@ async function importData() {
 async function deleteData() {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.error(err);
