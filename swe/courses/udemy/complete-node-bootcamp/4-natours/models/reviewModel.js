52936f6ea96e9aea9ab27c620dd42bc496cad993
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-21 22:58:37 -0700

    Implement calcAverageRating() functionality for review model

--- swe/courses/udemy/complete-node-bootcamp/4-natours/models/reviewModel.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 // review / rating / createdAt / tour ref / user ref
 const reviewSchema = new mongoose.Schema(
@@ -40,6 +41,48 @@ reviewSchema.pre(/^find/, function (next) {
   //   this.populate({ path: 'tour', select: 'name' });
   this.populate({ path: 'user', select: 'name photo' });
   next();
+});
+
+reviewSchema.statics.calcAvgerageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAvgerageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // query is executed to retrieve the review and passed to the post query handler
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() doesn't work here since query has already executed
+  await this.r.constructor.calcAvgerageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
