bf6c4b7c7207e08c460a29fbaeb901ad7005df58
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-22 07:51:14 -0700

    Add routes and logic for geospatial queries

--- swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
+++ swe/courses/udemy/complete-node-bootcamp/4-natours/controllers/tourController.js
@@ -1,5 +1,5 @@
 const { Tour } = require('../models');
-const { catchAsync } = require('../utils');
+const { AppError, catchAsync } = require('../utils');
 const { CODE, STATUS } = require('../constants');
 const factory = require('./handlerFactory');
 
@@ -96,3 +96,78 @@ exports.getTour = factory.getOne(Tour, {
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/34.127088,-118.113081/unit/mi
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  const [lat, lng] = latlng.split(',');
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please specify both the latitude and longitude in the format lat,lng',
+        CODE.BAD_REQUEST
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
+      },
+    },
+  });
+
+  res.status(CODE.OK).json({
+    status: STATUS.SUCCESS,
+    results: tours.length,
+    data: {
+      docs: tours,
+    },
+  });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+
+  const [lat, lng] = latlng.split(',');
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please specify both the latitude and longitude in the format lat,lng',
+        CODE.BAD_REQUEST
+      )
+    );
+  }
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(CODE.OK).json({
+    status: STATUS.SUCCESS,
+    data: {
+      docs: distances,
+    },
+  });
+});
